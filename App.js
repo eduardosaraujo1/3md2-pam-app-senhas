74db75de8bcd6b9eb4e83331576c8a0c332fda49
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2025-03-13 17:57:50 -0300

    feat: finished

--- App.js
+++ App.js
@@ -1,18 +1,77 @@
 import { Text, SafeAreaView, StyleSheet, View, ScrollView } from "react-native";
 import StyledButton from "./components/StyledButton";
+import { useState } from "react";
 
-export default function App() {
+function toPaddedInteger(num, padLen = 2) {
+    const numClamp = Math.max(0, Math.floor(num));
+
+    return numClamp.toString().padStart(padLen, "0");
+}
+
+/**
+ * @param {Array} stack The password type stack to convert to password number stack
+ */
+function getPassListFromStack(stack) {
+    const result = [];
+    const counts = { N: 0, P: 0, AP: 0 };
+
+    for (const item of stack) {
+        if (counts[item] === undefined) return;
+
+        counts[item]++;
+
+        if (item === "AP") {
+            result.push(item + "0" + toPaddedInteger(counts[item], 2));
+        } else {
+            result.push(item + "0" + counts[item]);
+        }
+    }
+
+    return result;
+}
+
+const App = () => {
+    const [passStack, setPassStack] = useState([]);
+
+    const pushPassword = (type = "N") => {
+        if (typeof type !== "string") return;
+        type = type.toUpperCase();
+
+        if (["N", "P", "AP"].includes(type)) {
+            setPassStack([...passStack, type]);
+        }
+    };
+
+    const passwordDisplay = getPassListFromStack(passStack).slice(-20).reverse();
+    const latestPass = passwordDisplay[0] ?? "Nenhum";
+
     return (
         <SafeAreaView style={styles.container}>
-            <View style={{ paddingInline: 8 }}>
+            <View>
+                <Text style={styles.heading}>Senha Atual</Text>
+                <Text style={{ fontSize: 36, textAlign: "center" }}>{latestPass}</Text>
+            </View>
+            <View style={{ paddingInline: 8, marginTop: 8 }}>
                 <View style={{ flexDirection: "row", gap: 16 }}>
-                    <StyledButton buttonStyle={styles.button} textStyle={styles.buttonText}>
+                    <StyledButton
+                        onPress={() => pushPassword("N")}
+                        buttonStyle={styles.button}
+                        textStyle={styles.buttonText}
+                    >
                         Normal
                     </StyledButton>
-                    <StyledButton buttonStyle={styles.button} textStyle={styles.buttonText}>
+                    <StyledButton
+                        onPress={() => pushPassword("P")}
+                        buttonStyle={styles.button}
+                        textStyle={styles.buttonText}
+                    >
                         Prioritário
                     </StyledButton>
-                    <StyledButton buttonStyle={styles.button} textStyle={styles.buttonText}>
+                    <StyledButton
+                        onPress={() => pushPassword("AP")}
+                        buttonStyle={styles.button}
+                        textStyle={styles.buttonText}
+                    >
                         Alta Prioridade
                     </StyledButton>
                 </View>
@@ -24,32 +83,22 @@ export default function App() {
 
                         maxHeight: 400,
                         marginTop: 16,
+                        padding: 8,
                     }}
                 >
-                    <Text
-                        style={{
-                            textAlign: "center",
-                            fontSize: 36,
-                            fontWeight: 700,
-                        }}
-                    >
-                        Senhas
-                    </Text>
-                    <ScrollView style={{ padding: 8 }}>
-                        <Text style={styles.passwordItem}>N01</Text>
-                        <Text style={styles.passwordItem}>P01</Text>
-                        <Text style={styles.passwordItem}>AP01</Text>
-                        <Text style={styles.passwordItem}>N01</Text>
-                        <Text style={styles.passwordItem}>P01</Text>
-                        <Text style={styles.passwordItem}>AP01</Text>
-                        <Text style={styles.passwordItem}>N01</Text>
-                        <Text style={styles.passwordItem}>P01</Text>
+                    <Text style={styles.heading}>Senhas</Text>
+                    <ScrollView>
+                        {passwordDisplay.map((item) => (
+                            <Text key={item} style={styles.passwordItem}>
+                                {item}
+                            </Text>
+                        ))}
                     </ScrollView>
                 </View>
             </View>
         </SafeAreaView>
     );
-}
+};
 
 const styles = StyleSheet.create({
     container: {
@@ -57,6 +106,11 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         backgroundColor: "#ecf0f1",
         padding: 8,
+    },
+    heading: {
+        textAlign: "center",
+        fontSize: 36,
+        fontWeight: 700,
     },
     button: {
         flex: 1,
@@ -71,3 +125,5 @@ const styles = StyleSheet.create({
         fontSize: 24,
     },
 });
+
+export default App;
